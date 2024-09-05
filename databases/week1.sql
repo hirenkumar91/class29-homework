e8d8ec5644eb58a854122235a333ea1063b15d3f
hirenkumar91 <hmtrivedi.3@gmail.com>
2024-09-05 20:25:16 +0200

    Find out how many tasks in the task table do not have a valid due date

--- databases/week1.sql
+++ databases/week1.sql
@@ -1,3 +1,9 @@
 -- Find out how many tasks are in the task table
 SELECT COUNT(*) AS total_tasks
 FROM task;
+
+--Find out how many tasks in the task table do not have a valid due date
+
+SELECT COUNT(*) AS invalid_due_date_count
+FROM task
+WHERE due_date IS NULL;
